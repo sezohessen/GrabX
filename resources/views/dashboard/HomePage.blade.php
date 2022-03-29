8d5969c5d43c061d113cab8f8da28e400bbef1ca
sezohessen <hu.elshazly@nu.edu.eg>
2022-03-29 03:35:29 +0200

    adding login

---
+++ resources/views/dashboard/HomePage.blade.php
@@ -0,0 +1,7 @@
+@extends('dashboard.layouts.app')
+
+@section('content')
+    <div>
+        <h1>test</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {return view('dashboard.HomePage'); });
 });
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
