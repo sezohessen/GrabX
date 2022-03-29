bcda492653544659caf908d95ce8c747a0e9ee67
sezohessen <hu.elshazly@nu.edu.eg>
2022-03-29 06:24:20 +0200

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,3 @@ Route::middleware(['auth'])->group(function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
