5d97082a229301b7fc3c1e76991d0705fa1cbbf5
sezohessen <hu.elshazly@nu.edu.eg>
2022-04-04 07:55:31 +0200

    Order Seeders

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    const status = [
+        0   => 'pending',
+        1   => 'on the way',
+        2   => 'deliverled',
+        3   => 'canceled'
+    ];
+    const unit_type = [
+        0   => 'house',
+        1   => 'appartment',
+        2   => 'office',
+    ];
     protected $table    = 'orders';
     protected $fillable=[
         'name',
--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Governorate;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\ProductAdditionalOptionItem;
+use App\Models\ProductMultipleSelectItem;
+use App\Models\ProductSelectOptionItem;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
@@ -13,8 +18,163 @@ class OrderSeeder extends Seeder
      *
      * @return void
      */
+    public function seedProductOption($table1,$table2,$column,$products){
+        $faker          = Faker::create();
+        $faker_ar       = Faker::create('ar_SA');
+        /* Make Select option For products */
+        for ($i = 0; $i < 2; $i++) {
+            $idOption     = DB::table($table1)->insertGetId([
+                'name'          => $faker->name,
+                'name_ar'       => $faker_ar->name,
+                'product_id'    => $product = $products->random()->id,
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ]);
+            for ($j = 0; $j < 2; $j++) {
+                DB::table($table2)->insert([
+                    'name'          => $faker->name,
+                    'name_ar'       => $faker_ar->name,
+                    'price'         => $faker->numberBetween(1,5),
+                    'product_id'    => $product,
+                    $column         => $idOption,
+                    'created_at'    => now(),
+                    'updated_at'    => now(),
+                ]);
+            }
+        }
+    }
+    public function seedOrderProductOption($table,$orderID,$productID,$columnName,$optionIDs,$qty = NULL)
+    {
+        $faker          = Faker::create();
+        if($qty){
+            for ($j = 0; $j < 2; $j++) {
+                DB::table($table)->insert([
+                    'order_id'          => $orderID,
+                    'product_id'        => $productID,
+                    $columnName         => $optionIDs->random()->id,
+                    'qty'               => $faker->numberBetween(1,4),
+                    'created_at'        => now(),
+                    'updated_at'        => now(),
+                ]);
+            }
+        }else{
+            for ($j = 0; $j < 2; $j++) {
+                DB::table($table)->insert([
+                    'order_id'          => $orderID,
+                    'product_id'        => $productID,
+                    $columnName         => $optionIDs->random()->id,
+                    'created_at'        => now(),
+                    'updated_at'        => now(),
+                ]);
+            }
+        }
+
+    }
     public function run()
     {
-        
+        $faker          = Faker::create();
+        $faker_ar       = Faker::create('ar_SA');
+        $governorates   = Governorate::all();
+        $status         = Order::status;
+        $unit_type      = Order::unit_type;
+        $products       = Product::all();
+
+        /* Prepare for Orders */
+
+        $this->seedProductOption(
+            'product_select_options',
+            'product_select_option_items',
+            'product_select_option_id',
+            $products
+        );
+        $this->seedProductOption(
+            'product_multiple_selects',
+            'product_multiple_select_items',
+            'product_multiple_select_id',
+            $products
+        );
+        $this->seedProductOption(
+            'product_additional_options',
+            'product_additional_option_items',
+            'product_select_option_id',
+            $products
+        );
+        $product_option     = ProductSelectOptionItem::all();
+        $product_muloption  = ProductMultipleSelectItem::all();
+        $product_addition   = ProductAdditionalOptionItem::all();
+
+        /* Make Order Seeder with way of order(Pickup,Deliverly ) with it's information and order items details */
+        for ($i = 0; $i < 10; $i++) {
+            $id     = DB::table('orders')->insertGetId([
+                'name'              => $faker->name,
+                'phone'             => $faker->phoneNumber,
+                'email'             => $faker->boolean ? $faker->email: NULL,
+                'pickup'            => $existPickUP = ($faker->boolean) ? 1:0,
+                'deliverly'         => $existPickUP ? 0 : 1,
+                'discount'          => $faker->boolean ? $faker->numberBetween(5,80): 0,
+                'subtotal'          => $price = $faker->numberBetween(5,150),
+                'deliverly_cost'    => $existPickUP ? 0 : $faker->numberBetween(1,10),
+                'total'             => $faker->numberBetween($price,151),
+                'status'            => array_rand($status),
+                'created_at'        => now(),
+                'updated_at'        => now(),
+            ]);
+            if($existPickUP){
+                DB::table('order_pickups')->insert([
+                    'order_id'      => $id,
+                    'make'          => $faker->countryCode,
+                    'color'         => $faker->colorName,
+                    'license'       => $faker->name,
+                    'created_at'    => now(),
+                    'updated_at'    => now(),
+                ]);
+            }else{
+                $governorate = $governorates->random();
+                DB::table('order_deliverlies')->insert([
+                    'order_id'          => $id,
+                    'governorate_id'    => $governorate->id,
+                    'city_id'           => $governorate->cities->random()->id,
+                    'unit_type'         => array_rand($unit_type),
+                    'street'            => $faker->city,
+                    'house_num'         => $faker->numberBetween(1,10),
+                    'special_direction' => $faker->city,
+                    'created_at'        => now(),
+                    'updated_at'        => now(),
+                ]);
+            }
+            for ($j = 0; $j < 3; $j++) {
+                DB::table('order_items')->insert([
+                    'order_id'          => $id,
+                    'product_id'        => $product = $products->random()->id,
+                    'qty'               => $faker->numberBetween(1,4),
+                    'price'             => $faker->numberBetween(1,50),
+                    'created_at'        => now(),
+                    'updated_at'        => now(),
+                ]);
+                $this->seedOrderProductOption(
+                    'order_item_options',
+                    $id,
+                    $product,
+                    'product_select_option_item_id',
+                    $product_option
+                );
+                $this->seedOrderProductOption(
+                    'order_product_multiple_selects',
+                    $id,
+                    $product,
+                    'product_item_id',
+                    $product_muloption
+                );
+                $this->seedOrderProductOption(
+                    'order_product_additional_option_items',
+                    $id,
+                    $product,
+                    'product_item_id',
+                    $product_addition,
+                    $qty = 1
+                );
+            }
+
+        }
     }
 }
