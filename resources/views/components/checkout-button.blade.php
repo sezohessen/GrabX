677caf4edf174d21b7e9463075369f793e350d10
sezohessen <hu.elshazly@nu.edu.eg>
2022-04-17 22:49:42 +0200

    adjust css

--- resources/views/components/checkout-button.blade.php
+++ resources/views/components/checkout-button.blade.php
@@ -1,3 +1,4 @@
-<div class="checkout-button">
-        <a href="#"> @lang('Checkout') </a>
+<div style="position: relative">
+    <button class="w-100 btn btn-danger btn-lg"> {{ $message }} </button>
 </div>
+
