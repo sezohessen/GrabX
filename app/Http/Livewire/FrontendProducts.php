c13c43aa2204b7391d7ae19359766ad03f51bf99
sezohessen <hu.elshazly@nu.edu.eg>
2022-04-18 07:33:51 +0200

    add to cart in products

--- app/Http/Livewire/FrontendProducts.php
+++ app/Http/Livewire/FrontendProducts.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Cart;
+use App\Models\CartItem;
+use App\Models\CartItemOption;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\ProductSelectOptionItem;
 use Livewire\Component;
 use Livewire\WithPagination;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request as FacadesRequest;
 class FrontendProducts extends Component
 {
 
@@ -17,6 +23,8 @@ class FrontendProducts extends Component
     public    $search      = '';
     public    $page        = 1;
     private   $pagination  = 10;
+    public    $total;
+
 
     public function mount($id)
     {
@@ -31,8 +39,36 @@ class FrontendProducts extends Component
     }
 
     // Add to cart
-    public function addToCart()
+    public function addToCart(Request $request,$id)
     {
-        
+        $ip         = FacadesRequest::ip();
+        $product    = Product::findOrFail($id);
+        $cart       = Cart::where('ip',$ip)->first();
+        // $isExist    =  $cart? 1 : 0;
+        $hasOption  = ProductSelectOptionItem::where('product_id',$product->id)->first();
+
+        if(!$hasOption)
+        {
+            if($cart){
+                $newCart    = Cart::updateOrCreate([
+                   'ip'         => $ip,
+                   'subtotal'   => 0,
+                   'total'      => $this->total,
+                ]);
+                CartItem::create([
+                    'cart_id'       => $newCart->id,
+                    'product_id'    => $product->id,
+                    'qty'           => ($request->quantity) ? $request->quantity : 1,
+                    'price'         => $product->price,
+                ]);
+                $this->total = $product->price * $request->quantity;
+                dd($this->total);
+                session()->flash('add', __('Product has successfully been added to cart'));
+            }
+        } else {
+            return redirect()->route('tenant.Product',['id' => $product->id]);
+        }
+
+
     }
 }
