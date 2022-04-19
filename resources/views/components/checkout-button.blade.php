25768a5c472d7a432f4f6453402f01272e269c6e
sezohessen <hu.elshazly@nu.edu.eg>
2022-04-19 06:29:59 +0200

    Merge livwire

--- resources/views/components/checkout-button.blade.php
+++ resources/views/components/checkout-button.blade.php
@@ -1,4 +1,6 @@
 <div style="position: relative">
-    <button class="w-100 btn btn-danger btn-lg"> {{ $message }} </button>
+    <a href="{{ route('tenant.cart.show') }}">
+        <button class="w-100 btn btn-danger btn-lg"> {{ $message }} </button>
+    </a>
 </div>
 
